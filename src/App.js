3a9f9b0de62db3c994954efbfa7dd8c2c8288ca8
Mehmet Demircioglu <demircioglumhmt@gmail.com>
2022-02-02 14:43:22 +0300

    chartbar add component

--- src/App.js
+++ src/App.js
@@ -9,19 +9,19 @@ const DUMMY_DATA = [
   {
     id:"1",
     title:"Bilgisayar",
-    amount:95.12,
-    date: new Date(2022,12,1)
+    amount:25.12,
+    date: new Date(2022,1,12)
   },
   {
     id:"2",
     title:"Bilgisayar 2",
-    amount:95.12,
-    date: new Date(2022,15,2)
+    amount:12.12,
+    date: new Date(2022,2,15)
   },
   {
     id:"3",
     title:"Bilgisayar",
-    amount:95.12,
+    amount:70.12,
     date: new Date(2022,6,6)
   },
   {
@@ -29,6 +29,18 @@ const DUMMY_DATA = [
     title:"Bilgisayar",
     amount:95.12,
     date: new Date(2022,7,5)
+  },
+  {
+    id:"4",
+    title:"Bilgisayar",
+    amount:95.12,
+    date: new Date(2020,3,5)
+  },
+  {
+    id:"4",
+    title:"Bilgisayar",
+    amount:95.12,
+    date: new Date(2020,4,5)
   },
 
 ];
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React,{useState} from "react";
 import Card from "../UI/Card";
 
 import './Expenses.css';
+import ExpensesChart from "./ExpensesChart";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
 
@@ -16,11 +17,16 @@ const Expenses = (props) => {
         setFilteredYear(value)
     }
 
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    })
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filteredChangeFilter} />
-                <ExpensesList items={props.items} />
+                <ExpensesChart expenses = {filteredExpenses} />
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     )
